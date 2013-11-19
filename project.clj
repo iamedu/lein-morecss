6a4c3c01cf1607f04c1b60494f37eab068dbea6e
Eduardo Díaz <iamedu@gmail.com>
2013-11-19 16:52:42 -0600

    Changing name to lein-morecss

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject morecss "0.1.0-SNAPSHOT"
+(defproject lein-morecss "0.1.0-SNAPSHOT"
   :description "Compile several less files into a single lesscss"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/iamedu/lein-morecss"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[com.asual.lesscss/lesscss-engine "1.4.2"]
